4900180dabe65261e1271c32dc6ff96c60820599
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-08-17 08:17:54 -0300

    Update Item.php

--- gerenciador de tarefas final/models/Item.php
+++ gerenciador de tarefas final/models/Item.php
@@ -3,33 +3,33 @@
 class Item {
     private $nome;
     private $concluido;
-	private $prioridade;
+    private $prioridade;
 
     public function __construct($nome) { 
         $this->nome = $nome;
         $this->concluido = false;
-		$this->prioridade = 0;
+	$this->prioridade = 0;
     }
     
-	public function getNome() {
-		return $this->nome;
-	}
+    public function getNome() {
+	return $this->nome;
+    }
 	
-	public function setNome($nome): self {
-		$this->nome = $nome;
-		return $this;
-	}
-
-	public function getConcluido() {
-		return $this->concluido;
-	}
+    public function setNome($nome): self {
+	$this->nome = $nome;
+	return $this;
+    }
 	
-	public function setConcluido($concluido): self {
-		$this->concluido = $concluido;
-		return $this;
-	}
+    public function getConcluido() {
+	return $this->concluido;
+    }
 	
-	public function getPrioridade() {
+    public function setConcluido($concluido): self {
+	$this->concluido = $concluido;
+	return $this;
+    }
+	
+    public function getPrioridade() {
         return $this->prioridade;
     }
 
