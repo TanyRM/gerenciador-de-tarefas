88ef70bee3daad7843c7238e9f43efaa65583fc9
TanyRM <tanielemadu@outlook.com>
2024-06-03 17:23:38 -0300

    Limpeza de codigo

--- web/java_spring/src/main/java/com/taniele/java_spring/controller/GerenciadorController.java
+++ web/java_spring/src/main/java/com/taniele/java_spring/controller/GerenciadorController.java
@@ -4,12 +4,9 @@ import com.taniele.java_spring.entity.Usuario;
 import com.taniele.java_spring.services.GerenciadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class GerenciadorController {
