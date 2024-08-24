95d57c4b2eb9e8d6db6ac76aee7f615566f9e014
Taniele <127350142+TanyRM@users.noreply.github.com>
2024-08-24 20:37:11 -0300

    Delete web/java_spring directory

--- web/java_spring/src/main/java/com/taniele/java_spring/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taniele.java_spring.repository;
-
-import com.taniele.java_spring.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- web/java_spring/src/main/java/com/taniele/java_spring/services/ItemService.java
+++
@@ -1,9 +0,0 @@
-package com.taniele.java_spring.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ItemService {
-
-
-}
